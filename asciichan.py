45ca33b22dfecba3b17e587f0783e785072c7418
Rohit Date <rohitdate86@gmail.com>
2016-02-17 15:51:50 -0800

    asciichan styling etc

--- asciichan.py
+++ asciichan.py
@@ -19,8 +19,10 @@ import webapp2
 import cgi
 import jinja2 
 
+from google.appengine.ext import db
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 class Handler (webapp2.RequestHandler):
     def write (self, *a, **kw):
@@ -33,6 +35,32 @@ class Handler (webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+class Art(db.Model):
+	title = db.StringProperty(required=True)
+	art = db.TextProperty(required=True)
+
+	created = db.DateTimeProperty(auto_now_add=True)
+
+
+
 class AsciiChanHandler(Handler):
+
+	def render_front(self, title="", art="", error=""):
+		arts = db.GqlQuery("SELECT * FROM Art "
+						   "ORDER BY created DESC ")
+		self.render("asciichan.html", title=title, art=art, error=error, arts=arts)
+
 	def get(self):
-		self.render("asciichan.html")
+		self.render_front()
+
+	def post(self):
+		title = self.request.get("title")
+		art = self.request.get("art")
+
+		if title and art:
+			a = Art(title=title, art=art)
+			a.put()
+			self.redirect("/asciichan")
+		else:
+			error = "we need title and art"
+			self.render_front(error=error, title=title, art=art)
